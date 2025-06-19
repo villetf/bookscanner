3dc50780fab37bcb2005c3c174b9f2a8e9c98dba
Vilhelm Fontell <ville@fontell.se>
2025-06-19 18:03:13 +0200

    added scanner functionality

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# main.py -- put your code here!
+from machine import Pin, UART
+from time import sleep
+from buzzer import soundBuzzer
+from ledLight import powerOnLed
+
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+powerOnLed('yellow')
+
+print('Redo att skanna')
+
+
+while True:
+   if uart.any():
+      soundBuzzer(0.1)
+      data = uart.read()
+      data = data.decode()
+      print("Received:", data)
+   sleep(0.1)
+
