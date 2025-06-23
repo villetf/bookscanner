1f59add006e4fde6ec6498d9eba0fac91c08bc41
Vilhelm Fontell <ville@fontell.se>
2025-06-23 22:12:04 +0200

    added call to google books

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from machine import Pin, UART
 from time import sleep
 from buzzer import soundBuzzer
 from ledLight import powerOnLed
+from apiCalls import getGoogleBooksData
 
 uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
@@ -16,5 +17,6 @@ while True:
       data = uart.read()
       data = data.decode()
       print("Received:", data)
+      getGoogleBooksData(data)
    sleep(0.1)
 
