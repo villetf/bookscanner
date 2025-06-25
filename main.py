c1ad33b846da063943c9ea88ce572f9f53486e14
Vilhelm Fontell <ville@fontell.se>
2025-06-25 21:38:36 +0200

    added sending mqtt cal

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+import json
 from Book import Book
-from lib.mqttHelpers import connectToMQTT, sendTopic
+from constructBook import constructBook
+from mqttHelpers import connectToMQTT, sendTopic
 from machine import Pin, UART
 from time import sleep
 from buzzer import soundBuzzer
 from ledLight import powerOnLed
-from apiCalls import getGoogleBooksData
+from apiCalls import getGoogleBooksData, getOpenLibraryData
 
-uart = UART(0, baudrate=600, tx=Pin(0), rx=Pin(1))
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
 powerOnLed('yellow')
 
@@ -17,12 +19,21 @@ sendTopic('{"status": "readyTest"}', "devices/status", mqttConnection)
 
 
 while True:
+   if (not mqttConnection) or (not mqttConnection.is_connected()):
+      print("Reconnecting to MQTT...")
+      mqttConnection = connectToMQTT()
+
    if uart.any():
       soundBuzzer(0.1)
-      data = uart.read()
-      data = data.decode()
-      print("Received:", data)
-      googleData = getGoogleBooksData(data)
-      newBook = Book()
+      isbn = uart.read()
+      isbn = isbn.decode()
+      isbn = isbn.strip()
+      print("Received:", isbn)
+      googleData = getGoogleBooksData(isbn)
+      openLibraryData = getOpenLibraryData(isbn)
+      newBook = constructBook(isbn, googleData, openLibraryData)
+      jsonBook = json.dumps(newBook.__dict__).encode('utf-8')
+      print('längd på meddelande:', len(jsonBook))
+      sendTopic(jsonBook, "books/new", mqttConnection)
    sleep(0.1)
 
