0591d85ad1f65342b01bf3a250a230a36abf4e59
Vilhelm Fontell <ville@fontell.se>
2025-06-27 18:02:20 +0200

    added things

--- main.py
+++ main.py
@@ -8,21 +8,23 @@ from buzzer import soundBuzzer
 from ledLight import powerOnLed
 from apiCalls import getGoogleBooksData, getOpenLibraryData
 
-uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
+
+
+try:
+   mqttConnection = connectToMQTT()
+   soundBuzzer(0.1)
+   soundBuzzer(0.1)
+except:
+   print("Initial MQTT connection failed")
 
 powerOnLed('yellow')
-
 print('Redo att skanna')
 
-mqttConnection = connectToMQTT()
-sendTopic('{"status": "readyTest"}', "devices/status", mqttConnection)
 
+uart = UART(0, baudrate=9600, tx=Pin(0), rx=Pin(1))
 
 while True:
-   if (not mqttConnection) or (not mqttConnection.is_connected()):
-      print("Reconnecting to MQTT...")
-      mqttConnection = connectToMQTT()
-
+   powerOnLed('green')
    if uart.any():
       soundBuzzer(0.1)
       isbn = uart.read()
@@ -34,6 +36,9 @@ while True:
       newBook = constructBook(isbn, googleData, openLibraryData)
       jsonBook = json.dumps(newBook.__dict__).encode('utf-8')
       print('längd på meddelande:', len(jsonBook))
-      sendTopic(jsonBook, "books/new", mqttConnection)
+      try:
+          sendTopic(jsonBook, "books/new", mqttConnection)
+      except:
+          print("Failed sending book via MQTT")
    sleep(0.1)
 
